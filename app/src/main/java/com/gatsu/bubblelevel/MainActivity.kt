f86326894c68b9fa0ef046f620de6c02970d85db
Ypypy <kkkmit@mail.ru>
2022-02-25 12:36:05 +0300

    bubble level

--- app/src/main/java/com/gatsu/bubblelevel/MainActivity.kt
+++ app/src/main/java/com/gatsu/bubblelevel/MainActivity.kt
@@ -14,6 +14,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var sManager: SensorManager
 
+    private var magnetic = FloatArray(9)
+    private var gravity = FloatArray(9)
+
+    private var accrs = FloatArray(3)
+    private var magf = FloatArray(3)
+    private var values = FloatArray(3)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,12 +30,23 @@ class MainActivity : AppCompatActivity() {
         sManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
         val sensor = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val sensor2 = sManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
 
         val sListener = object : SensorEventListener{
-            override fun onSensorChanged(sEvent: SensorEvent?) {
-                val value = sEvent?.values
-                val sData = "X:${value?.get(0)}\nY:${value?.get(1)}\nZ:${value?.get(2)}"
-                binding.text.text = sData
+            override fun onSensorChanged(event: SensorEvent?) {
+               when(event?.sensor?.type){
+                   Sensor.TYPE_ACCELEROMETER -> accrs = event.values.clone()
+                   Sensor.TYPE_MAGNETIC_FIELD -> magf = event.values.clone()
+               }
+                SensorManager.getRotationMatrix(gravity,magnetic, accrs, magf)
+                val outGravity = FloatArray(9)
+                SensorManager.remapCoordinateSystem(gravity, SensorManager.AXIS_X, SensorManager.AXIS_Z,outGravity)
+
+                SensorManager.getOrientation(outGravity, values)
+                val degree = values[2] * 57.295779513f
+                val rotate = 270 + degree
+                binding.lRotation.rotation = rotate
+                binding.text.text = degree.toString()
             }
 
             override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
@@ -38,6 +56,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         sManager.registerListener(sListener, sensor,SensorManager.SENSOR_DELAY_NORMAL)
+        sManager.registerListener(sListener, sensor2,SensorManager.SENSOR_DELAY_NORMAL)
 
     }
 }
